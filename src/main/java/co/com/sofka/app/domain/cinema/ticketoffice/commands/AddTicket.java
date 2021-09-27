b1798c67bbe9ed74c0b5af6eb75293e45dea2aef
Jhon Serna <jhonalfaber@gmail.com>
2021-09-27 16:45:48 -0500

    creacion de commands y eventos para ticektOffice

---
+++ src/main/java/co/com/sofka/app/domain/cinema/ticketoffice/commands/AddTicket.java
@@ -0,0 +1,4 @@
+package co.com.sofka.app.domain.cinema.ticketoffice.commands;
+
+public class AddTicket {
+}
---
+++ src/main/java/co/com/sofka/app/domain/cinema/ticketoffice/events/UpdatedSeatNumberOfTicket.java
@@ -0,0 +1,4 @@
+package co.com.sofka.app.domain.cinema.ticketoffice.events;
+
+public class UpdatedSeatNumberOfTicket {
+}
